03bb8d63208668ea5a78cb8d2c9b1d728f7ed25f
Lorena <lorena.giddens@outlook.com>
2018-03-31 03:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = { prop: 1 };
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign ({}, object, {[key]: value });
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes.
+}
