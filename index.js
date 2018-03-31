aeaf456796cbda0b420906699cb14410deb5e860
Lorena <lorena.giddens@outlook.com>
2018-03-31 03:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign ({}, object, {[key]: value });
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  Object.key = value;
-  return Object;
+function destructivelyUpdateObjectWithKeyAndValue(recipes, prop2, '2') {
+  recipes.prop2 = 2;
+  return recipes;
 }
