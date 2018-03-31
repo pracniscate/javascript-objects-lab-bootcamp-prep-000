7b358d094b2d26db5427c6e565d4ec62a1cdabf4
Lorena <lorena.giddens@outlook.com>
2018-03-31 04:14:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete recipes[key];
-  return recipes;
+  delete object[key];
+  return object;
 }
